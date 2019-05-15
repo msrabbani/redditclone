b4d576e29f6afcc9e0f91f45952168c6f4d079fd
msrabbani <msrabbanii@gmail.com>
2019-05-15 16:53:42 +0700

    CR response

--- server/routes/threads.js
+++ server/routes/threads.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const threadCont = require('../controllers/threadController')
+const responseCont = require('../controllers/responseController')
 
 router.get('/', threadCont.get)
 router.get('/:id', threadCont.getOne)
@@ -9,5 +10,13 @@ router.put('/:id', threadCont.update)
 router.put('/:id/upvote', threadCont.upvote)
 router.put('/:id/downvote', threadCont.downvote)
 router.delete('/:id', threadCont.remove)
+
+router.get('/:id/reply', responseCont.get)
+router.get('/:id/reply/:repid', responseCont.getOne)
+router.post('/:id/reply', responseCont.create)
+router.put('/:id/reply/:repid', responseCont.update)
+router.put('/:id/reply/:repid/upvote', responseCont.upvote)
+router.put('/:id/reply/:repid/downvote', responseCont.downvote)
+router.delete('/:id/reply/:repid', responseCont.remove)
 
 module.exports = router
